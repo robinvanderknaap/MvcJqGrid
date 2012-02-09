052e683d38d3657c6f17dfee4fd10e07336e458e
kalmarg <gabor.kalmar@3dhistech.com>
2012-02-09 01:13:37 -0800

    issue 6

---
+++ src/MvcJqGrid.Tests/ModelBinderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MvcJqGrid.Tests
+{
+    [TestFixture]
+    public class ModelBinderTests
+    {
+        [Test]
+        public void CanFilterI18nCharacters()
+        {
+            var jsonFilter = "{\"groupOp\":\"AND\",\"rules\":[{\"field\":\"i18n\",\"op\":\"bw\",\"data\":\"árvíztűrőtükörfúrógépq\"}]}";
+            var filter = Filter.Create(jsonFilter);
+
+            Assert.IsInstanceOf<Filter>(filter);
+        }
+    }
+}
