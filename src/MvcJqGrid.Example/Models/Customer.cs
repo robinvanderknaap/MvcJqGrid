1a3f0fb12434baa8a117e519abffb1bcb9bd3dbc
Robin van der Knaap <robin@webpirates.nl>
2013-05-06 18:00:27 +0200

    Replaced sql server express with ravendb implementation in example project

---
+++ src/MvcJqGrid.Example/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MvcJqGrid.Example.Models
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string CustomerId
+        {
+            get
+            {
+                return Id.Replace("customers/", ""); // Remove ravendb prefix from identifier
+            }
+        } 
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname
+        {
+            get 
+            {
+                return String.IsNullOrWhiteSpace(Middlename) ?
+                    string.Format("{0} {1}", Firstname, Lastname) :
+                    string.Format("{0} {1} {2}", Firstname, Middlename, Lastname);
+            }
+        }
+        public string Company { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime LastModified { get; set; }
+        public string Telephone { get; set; }
+    }
+}
