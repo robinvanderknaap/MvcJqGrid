7cbff944c6d6a05064c690e5afeda2f08e3349d9
Zac Gross <zacgross@gmail.com>
2013-06-01 11:34:27 -0400

    Moving edit class files to new core project path

---
+++ src/MvcJqGrid/EditRules.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcJqGrid.Extensions;
+
+namespace MvcJqGrid
+{
+   
+    public class EditRules
+    {
+        public bool? EditHidden { get; set; }
+        public bool? Required { get; set; }
+        public bool? Number { get; set; }
+        public bool? Integer { get; set; }
+        public int? MinValue { get; set; }
+        public int? MaxValue { get; set; }
+        public bool? Email { get; set; }
+        public bool? Url { get; set; }
+        public bool? Date { get; set; }
+        public bool? Time { get; set; }
+        public bool? Custom { get; set; }
+        public string CustomFunc { get; set; }
+
+
+        public override string ToString()
+        {
+            return this.ToJSON();
+        }
+    }
+}
